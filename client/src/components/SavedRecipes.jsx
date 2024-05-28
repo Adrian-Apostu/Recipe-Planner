b534db00522c70751a7ba6b775ad44de49b073b3
Adrian Apostu <apostuadrian6@gmail.com>
2024-05-28 15:27:52 +0300

    fixed the css for saved recipes page

--- client/src/components/SavedRecipes.jsx
+++ client/src/components/SavedRecipes.jsx
@@ -10,16 +10,16 @@ export default function SavedRecipes() {
     }, []);
 
     return (
-        <div>
+        <div className="container mx-auto p-4 flex flex-col items-center">
             <h2 className="mt-8 text-xl font-bold">Saved Recipes</h2>
             {savedRecipes.length > 0 ? (
-                <ul className="mt-6">
+                <ul className="mt-6 w-full max-w-3xl">
                     {savedRecipes.map((recipe, index) => (
                         <RecipeCard key={index} recipe={recipe} isSavedPage />
                     ))}
                 </ul>
             ) : (
-                <p>No saved recipes.</p>
+                <p className="mt-6">No saved recipes.</p>
             )}
         </div>
     );
